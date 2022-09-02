c54be1ce32c7f30017a584167a58d8b9506f65dd
clod2008 <claudio@apsis.com.ar>
2022-09-02 13:06:28 -0300

    preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,14 @@ $(document).ready(function () {
 
     // Preloader
 
-    // $(window).load(function () { // makes sure the whole site is loaded
-    //     $('.page-preloader spinner').fadeOut(); // will first fade out the loading animation
-    //     $('.page-preloader').delay(350).fadeOut('slow');
-    //     // will fade out the white DIV that covers the website.
-    //     $('body').delay(350).css({
-    //         'overflow': 'visible'
-    //     });
-    // })
+    $(window).load(function () { // makes sure the whole site is loaded
+        $('.page-preloader spinner').fadeOut(); // will first fade out the loading animation
+        $('.page-preloader').delay(350).fadeOut('slow');
+        // will fade out the white DIV that covers the website.
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    })
 
     // Animated typing text
 
