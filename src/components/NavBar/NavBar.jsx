1425e1802d79e5b8acfbefc95ad5e7d6339d99f5
Claudio Uccello <claudio@apsis.com.ar>
2023-08-08 17:35:48 -0300

    custom Brand

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,53 +13,51 @@ export const NavBar = ( {navData, brand, expand} ) => {
 
     
   return (
-    <>
+  
+    <Navbar key={expand} expand={expand} className={`bg-body-tertiary mb-3 ${styles.mainNavBar}`}>
+      <Container fluid>
+        <NavLink to={'/'}>
+            <Navbar.Brand className={styles.customBrand}><Image src={brand} className='img-fluid' alt='DITP logo'/></Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+        <Navbar.Offcanvas
+            
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title className={styles.customBrand} id={`offcanvasNavbarLabel-expand-${expand}`}>
+              <Image src={brand} className='img-fluid' alt='DITP logo' />
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
 
-        <Navbar key={expand} expand={expand} className={`bg-body-tertiary mb-3 ${styles.mainNavBar}`}>
-          <Container fluid>
-            <NavLink to={'/'}>
-                <Navbar.Brand><Image src={brand} className='img-fluid' alt='DITP logo'/></Navbar.Brand>
-            </NavLink>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-                
-                id={`offcanvasNavbar-expand-${expand}`}
-                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  <Image src={brand} className='img-fluid' alt='DITP logo'/>
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-
-                <Nav className={`justify-content-end flex-grow-1 pe-3 ${styles.customOffCanvas}`}>
-                    {navData.map((item, id ) => 
-                        <Nav.Link
-                            key={id}
-                            className={styles.customNavLink}
-                            
+            <Nav className={`justify-content-end flex-grow-1 pe-3 ${styles.customOffCanvas}`}>
+                {navData.map((item, id ) => 
+                    <Nav.Link
+                        key={id}
+                        className={styles.customNavLink}
+                        
+                    >
+                        <NavLink
+                            to={item.link}
+                            className={ ({isActive }) => isActive ? styles.navActive : '' }
                         >
-                            <NavLink
-                                to={item.link}
-                                className={ ({isActive }) => isActive ? styles.navActive : '' }
-                            >
-                                { langSelected ==='en' ? item.text : item.textEs }
-                            </NavLink>
-                        </Nav.Link>
-                    )}
-                        <Nav.Link >  
-                            <SwitchButton label={langSelected} id={'lang'}/>
-                        </Nav.Link>
-                </Nav>
+                            { langSelected ==='en' ? item.text : item.textEs }
+                        </NavLink>
+                    </Nav.Link>
+                )}
+                    <Nav.Link >  
+                        <SwitchButton label={langSelected} id={'lang'}/>
+                    </Nav.Link>
+            </Nav>
 
 
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-   
-    </>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+
   )
 }
