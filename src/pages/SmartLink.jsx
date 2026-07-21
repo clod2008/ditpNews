e86af148b256e1e1160c6d53672c7628b56bed71
clod2008 <claudio@apsis.com.ar>
2026-07-21 18:33:23 -0300

    Update

--- src/pages/SmartLink.jsx
+++ src/pages/SmartLink.jsx
@@ -263,9 +263,7 @@ const SmartLinkBuilder = () => {
     <div className={styles.builder}>
       <img src={ditpIso} alt="DITP" className={styles.builderLogo} />
       <h1 className={styles.builderTitle}>Constructor de Smart Link a Instagram</h1>
-      <p className={styles.builderHint}>
-        Todavía no hay un posteo asignado a este link. Armá uno nuevo:
-      </p>
+
 
       <ScanStats />
 
